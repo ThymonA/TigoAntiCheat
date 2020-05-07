267288f5ead11ed9aca062705167d2822f4d5be8
mindiw dev <mindiw01@gmail.com>
2020-05-07 13:06:13 -0300

    Was added new locale (locale/es.lua)

---
+++ locales/es.lua
@@ -0,0 +1,25 @@
+AntiCheat.Locales['es'] = {
+    ['checking'] = '👮 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | Estás siendo verificado...',
+    ['empty_reason'] = 'No se especifico una razón',
+    ['resource_starting'] = '👮 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | Esta iniciandose, por favor se paciente....',
+    ['unknown_error'] = '🛑 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | No puedes ingresar dado un error, intenta nuevamente..',
+    ['country_not_allowed'] = '🛑 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | Tu pais {{{country}}} no está permitido ingresar al servidor.',
+    ['blocked_ip'] = '🛑 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | Tu IP está en la lista negra, esto se debe por utilizar una VPN o actividades sospechosas.',
+    ['banned'] = '🛑 𝗧𝗶𝗴𝗼𝗔𝗻𝘁𝗶𝗖𝗵𝗲𝗮𝘁 | Estas baneado del servidor ( Usuario: {{{username}}} )',
+    ['new_identifiers'] = 'Nuevos identificadores encontrados',
+    ['ban_type_godmode'] = 'Se detecto godmode en el usuario',
+    ['ban_type_injection'] = 'El jugador injecto comandos (Injection)',
+    ['ban_type_blacklisted_weapon'] = 'El jugador tiene un arma de la lista negra: {{{item}}}',
+    ['ban_type_blacklisted_key'] = 'El jugador presiono una tecla de la lista negra: {{{item}}}',
+    ['ban_type_hash'] = 'El jugador modifico un hash.',
+    ['ban_type_esx_shared'] = 'El jugador ejecuto el evento esx:getSharedObject',
+    ['ban_type_superjump'] = 'El jugador tiene su salto modificado.',
+    ['ban_type_client_files_blocked'] = 'El jugador no respondio despues de 5 veces si todavía estaba vivo (Client Files Blocked)',
+    ['ban_type_event'] = 'El jugador intento llamar al evento \'{{{event}}}\'',
+
+    ['none'] = 'Ninguno',
+
+    -- Discord
+    ['discord_title'] = '[TigoAntiCheat] Baneo un jugador',
+    ['discord_description'] = '**Nombre:** {{{name}}}\n **Razón:** {{{reason}}}\n **Identificadores:**\n {{{identifiers}}}\n **Otros identificadores:**\n {{{matchingIdentifiers}}}'
+}
